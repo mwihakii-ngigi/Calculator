9ac0c4ae2ee772d8b33265b0af4607f421f8a78f
MWIHAKI NGIGI <ngigileah92@gmail.com>
2024-08-28 13:46:53 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+class MainApp(App):
+    def build(self):
+        self.operators = ["/", "*", "+", "-"]
+
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.run()
